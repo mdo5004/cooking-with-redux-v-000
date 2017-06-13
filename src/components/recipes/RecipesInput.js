a0823a5e28d94440e566ebb43c58a62559fb353f
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-06-13 13:20:48 -0400

    Almost done

--- src/components/recipes/RecipesInput.js
+++ src/components/recipes/RecipesInput.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import { addRecipe } from '../../actions/recipes'
+import {AddIngredients} from '../ingredients/AddIngredients'
+
+
 export class RecipesInput extends Component {
-    
+
     constructor(){
         super();
         this.state={
@@ -15,27 +18,30 @@ export class RecipesInput extends Component {
         this.props.addRecipe(this.state)
     }
     handleOnChange = (e,field) => {
-        switch (field){
-            case 'name':
-                this.setState({
-                    ...this.state,
-                    name: e.target.value,
-                })
-                break;
-            case 'calories':
-                this.setState({
-                    ...this.state,
-                    calories: e.target.value,
-                })
-                break;
+        if (field === "name"){
+            this.setState({
+                ...this.state,
+                name: e.target.value,
+            })
+        } else {
+            this.setState({
+                ...this.state,
+                calories: e.target.value,
+            })
         }
     }
     render(){
         return(
             <div>
                 <form onSubmit={this.handleOnSubmit}>
-                    <input type='text' onChange={ event => this.handleOnChange(event,'name')} value={this.state.name} ></input>
-                    <input type='text' onChange={ event => this.handleOnChange(event,'calories')} value={this.state.calories} ></input>
+                    <label>Recipe
+                        <input type='text' onChange={ event => this.handleOnChange(event,'name')} value={this.state.name} placeholder="Spaghetti and Meatballs"></input>
+                    </label>
+                    <label>Calories
+                        <input type='text' onChange={ event => this.handleOnChange(event,'calories')} value={this.state.calories} placeholder="0"></input>
+                    </label>
+                    <AddIngredients />
+                    <button type="submit">Create Recipe</button>
                 </form>
             </div>
         )
