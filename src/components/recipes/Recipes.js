5551eeaf2bde36406756786f6ce55bb52bdf5ed1
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-06-13 13:12:57 -0400

    Passing all but addIngredients

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
 
 export class Recipes extends Component {
   render(){
+      const recipes = this.props.recipes.map( (recipe,index) => {
+          return <li key={index}>{recipe.name}</li>
+      })
     return(
         <div>
           <ul>
-            Recipes
+            {recipes}
           </ul>
         </div>
     )
   }
 }
 
-
-export const ConnectedRecipes = (Recipes)
+const mapStateToProps = (state) => {
+    return {
+        recipes: state.recipes
+    }
+}
+export const ConnectedRecipes = connect(mapStateToProps)(Recipes)
